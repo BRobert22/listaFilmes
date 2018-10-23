23767e3a328e8af599ebcdf21ac6f885c1402d81
robertb.guimaraes@hotmail.com <sUfx=zDX,Bb4C]6eA.zYJQc?dB*kT->
2018-10-22 22:11:08 -0300

    start commit

--- app/src/main/java/com/example/robert/listafilmes/ListaFilmes.kt
+++ app/src/main/java/com/example/robert/listafilmes/ListaFilmes.kt
@@ -7,38 +7,28 @@ import kotlinx.android.synthetic.main.activity_lista_filmes.*
 
 class ListaFilmes : AppCompatActivity() {
 
-    var languages = arrayOf(
-        "English",
-        "French",
-        "Spanish",
-        "Hindi",
-        "Russian",
-        "Telugu",
-        "Chinese",
-        "German",
-        "Portuguese",
-        "Arabic",
-        "Dutch",
-        "Urdu",
-        "Italian",
-        "Tamil",
-        "Persian",
-        "Turkish",
-        "Other"
+    var lstFilmes = arrayOf(
+        getText(R.string.nome_sonhe_liberdade),
+        getText(R.string.nome_godfather),
+        getText(R.string.nome_godfather_2),
+        getText(R.string.nome_pulp_fiction),
+        getText(R.string.nome_clube_luta),
+        getText(R.string.nome_forrest_gump),
+        getText(R.string.nome_matrix),
+        getText(R.string.nome_vida_bela)
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_filmes)
 
-
-
         // Create an ArrayAdapter using a simple spinner layout and languages array
-        val aa = ArrayAdapter(this, android.R.layout.simple_spinner_item, languages)
+        val aa = ArrayAdapter(this, android.R.layout.simple_spinner_item, lstFilmes)
         // Set layout to use when the list of choices appear
         aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         // Set Adapter to Spinner
         spinnerFilmes.setAdapter(aa)
+
     }
 
 }
